0b6632099c5171e9e790a432785d911e0bb79b78
Abdullah29804 <156453410+Abdullah29804@users.noreply.github.com>
2024-02-17 14:59:56 +0500

    Add files via upload

---
+++ Hello World/app.js
@@ -0,0 +1,17 @@
+// Variables declared by using var.
+// prompt is used to get input from the user.
+// alert() display message in a dialog box that pops up on the screen.
+// console.log() outputs message to the console.
+// document.write() outputs message to HTML web pages (Browser Window).
+// To create a line break in JavaScript, use "<br>".
+
+
+var a = prompt("Enter your name:");
+alert(a);
+document.write(a + "<br>");
+console.log(a);
+var question = "What is your age?";
+var age = prompt(question);
+alert(age);
+document.write(age);
+console.log(age);
